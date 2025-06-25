0c4630b39ebacb0b7ddf0ef46f13b3040221ef36
john0504 <john0504@ya.ru>
2025-06-25 14:15:27 +0300

    add throttleAndDebounce.js

---
+++ throttleAndDebounce.js
@@ -0,0 +1,72 @@
+function debounce(f, ms) {
+  let timerId = null;
+
+  return function (...args) {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => f.apply(this, args), ms);
+  };
+};
+
+function throttle(f, ms) {
+  let isThrottled = false;
+  let lastArgs = null;
+  let lastThis = null;
+
+  return function(...args) {
+    lastArgs = args;
+    lastThis = this;
+
+    if (isThrottled) {
+      return;
+    };
+
+    isThrottled = true;
+
+    setTimeout(() => {
+      f.apply(lastThis, lastArgs);
+      isThrottled = false;
+      lastArgs = null;
+      lastThis = null;
+    }, ms);
+  };
+}
+
+function f(text, ms) {
+  console.log(text, ms, ', this = ', this);
+};
+
+let obj = {
+  a: 1,
+  tF: throttle(f, 1000),
+  dF: debounce(f, 1000),
+}
+
+setTimeout(() => obj.tF('tF', 0), 0);
+setTimeout(() => obj.tF('tF', 400), 400);
+setTimeout(() => obj.tF('tF', 800), 800);
+setTimeout(() => obj.tF('tF', 1200), 1200);
+setTimeout(() => obj.tF('tF', 1600), 1600);
+setTimeout(() => obj.tF('tF', 2000), 2000);
+setTimeout(() => obj.tF('tF', 2400), 2400);
+setTimeout(() => obj.tF('tF', 2800), 2800);
+setTimeout(() => obj.tF('tF', 3200), 3200);
+setTimeout(() => obj.tF('tF', 3600), 3600);
+
+// // test on browser
+// let start = Date.now();
+
+// let obj2 = {
+//   e: 1,
+//   tF: throttle(
+//     function(e) {console.log('throttle:', 'x = ', e.clientX, ', diff = ', Date.now() - start, ', this = ', this)},
+//     1000
+//   ),
+//   dF: debounce(
+//     function(e) {console.log('debounce:', 'x = ', e.clientX, ', diff = ', Date.now() - start, ', this = ', this)},
+//     1000
+//   ),
+// };
+
+// document.body.addEventListener('mousemove', e => obj2.tF(e));
+
+// document.body.addEventListener('mousemove', e => obj2.dF(e));
