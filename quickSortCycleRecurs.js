fcc102d91e5b6731efa7592c863b27cfe7560abc
john0504 <john0504@ya.ru>
2025-06-27 18:09:07 +0300

    fix

--- quickSortCycleRecurs.js
+++ quickSortCycleRecurs.js
@@ -6,71 +6,73 @@ console.log('arr = ', arr);
 console.log('-------------------с помощью рекурсии--------------------------------');
 let count = 0;
 function quickSort(array) {
-    if (array.length <= 1) {
-        return array
+  if (array.length <= 1) {
+    return array;
+  };
+
+  let pivotIndex = Math.floor(array.length / 2);
+  let pivot = array[pivotIndex];
+  let less = [];
+  let greater = [];
+  for (let i = 0; i < array.length; i++) {
+    count += 1;
+    if(i === pivotIndex)
+      continue
+    if (array[i] < pivot) {
+      less.push(array[i]);
+    } else {
+      greater.push(array[i]);
     }
-    let pivotIndex = Math.floor(array.length / 2);
-    let pivot = array[pivotIndex]
-    let less = []
-    let greater = []
-    for (let i = 0; i < array.length; i++) {
-        count += 1
-        if(i === pivotIndex)
-            continue
-        if (array[i] < pivot) {
-            less.push(array[i])
-        } else {
-            greater.push(array[i])
-        }
-    }
-    return [...quickSort(less), pivot, ...quickSort(greater)]
+  }
+
+  return [...quickSort(less), pivot, ...quickSort(greater)];
 }
 
-console.log('quickSort: result = ',quickSort(arr))
-console.log('quickSort: count = ', count)
+console.log('quickSort: result = ',quickSort(arr));
+console.log('quickSort: count = ', count);
 
 console.log('-------------------с помощью цикла--------------------------------');
 count = 0
 function quickSortCycle(array) {
-    if (array.length <= 1) {
-        return array
-    }
+  if (array.length <= 1) {
+    return array;
+  }
 
-    let stack = [array];
-    let result = [];
+  let stack = [array];
+  let result = [];
 
-    while (stack.length) {
-        let curArr = stack.pop();
+  while (stack.length) {
+    let curArr = stack.pop();
 
-        if (curArr.length === 1) {
-            result.push(...curArr);
-            continue;
-        };
-
-
-        let pivotIndex = Math.floor(curArr.length / 2);
-        let pivot = curArr[pivotIndex]
-        let less = []
-        let greater = []
-
-        for (let i = 0; i < curArr.length; i++) {
-            count += 1
-            if(i === pivotIndex)
-                continue;
-            if (curArr[i] < pivot) {
-                less.push(curArr[i])
-            } else {
-                greater.push(curArr[i])
-            }
-        }
-
-        less.length && stack.push(less);
-        stack.push([pivot]);
-        greater.length && stack.push(greater);
-        // stack = [array] -> [less , [pivot], greater] -> [less , [pivot], lessNext, [pivotNext], greaterNext]
+    if (curArr.length === 1) {
+      result.push(...curArr);
+      continue;
     };
 
-    return result.reverse(); // result = [9,8,7 ... 1] - нужен реверс
+
+    let pivotIndex = Math.floor(curArr.length / 2);
+    let pivot = curArr[pivotIndex];
+    let less = [];
+    let greater = [];
+
+    for (let i = 0; i < curArr.length; i++) {
+      count += 1;
+      if(i === pivotIndex)
+        continue;
+      if (curArr[i] < pivot) {
+        less.push(curArr[i]);
+      } else {
+        greater.push(curArr[i]);
+      };
+    };
+
+    less.length && stack.push(less);
+    stack.push([pivot]);
+    greater.length && stack.push(greater);
+    // stack = [array] -> [less , [pivot], greater] -> [less , [pivot], lessNext, [pivotNext], greaterNext]
+  };
+
+  return result.reverse(); // result = [9,8,7 ... 1] - нужен реверс
 }
 
 console.log('quickSortCycle: result = ', quickSortCycle(arr))
